a0a8583a0a18a1b0a82de9d26bad99b0ac18d671
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2021-04-01 20:44:36 +0200

    first commit

---
+++ src/test/java/com/mediscreen/infospatient/InfospatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.infospatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfospatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
