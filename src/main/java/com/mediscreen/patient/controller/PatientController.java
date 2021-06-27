5986866bf8e9ec19b8062da72040371a39938ac5
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2021-06-27 13:38:21 +0200

    add logger

--- src/main/java/com/mediscreen/patient/controller/PatientController.java
+++ src/main/java/com/mediscreen/patient/controller/PatientController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.mediscreen.patient.model.Patient;
 import com.mediscreen.patient.service.PatientService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -22,13 +24,17 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @CrossOrigin("*")
 public class PatientController {
 
+    Logger logger = LoggerFactory.getLogger(PatientController.class);
+
     @Autowired
     PatientService patientService;
     
     @GetMapping(path = "/patients")
     public ResponseEntity<List<Patient>> listAllPatients() {
+        logger.info("get all patients.");
         List<Patient> patients = patientService.listAll();
         if(patients == null) {
+            logger.warn("No patient found");
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.ok().body(patients);
@@ -37,10 +43,11 @@ public class PatientController {
 
     @PostMapping(path = "/patient/add")
     public ResponseEntity<Patient> createPatient(@Validated @RequestBody Patient patient) {
-           
+           logger.info("saving patient: "+patient);
             Patient patientAdded = patientService.savePatient(patient);
 
             if(patientAdded == null) {
+                logger.warn("no patient added, patient is null");
                 return ResponseEntity.noContent().build();
             }
 
@@ -56,10 +63,11 @@ public class PatientController {
    
     @GetMapping(path = "/patient/{id}")
     public ResponseEntity<Patient> getPatientById(@PathVariable int id) {
-       
+       logger.info("get patient: "+id);
         Patient patient = patientService.findById(id);
        
         if(patient == null) {
+            logger.warn("bad request, patient is null.");
             return ResponseEntity.badRequest().build();
         } else {
             return ResponseEntity.ok().body(patient);
@@ -68,10 +76,11 @@ public class PatientController {
 
     @PutMapping(path = "/patient/{id}")
     public ResponseEntity<Patient> updatePatient(@PathVariable("id") int id, @RequestBody Patient patientWithUpdate){
-        
+        logger.info("update patient: "+id);
         Patient patient = patientService.findById(id);
        
         if(patient == null || patientWithUpdate == null) {
+            logger.warn("bad request, patient is null.");
             return ResponseEntity.badRequest().build();
         }
 
