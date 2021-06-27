3cd36d4f5c85612659ef2b8ab8437a90dc2d4d3f
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2021-06-27 14:48:34 +0200

    données de départ modifiées

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
 INSERT INTO `patient` (`id`, `address`, `birthdate`, `first_name`, `gender`, `last_name`, `phone_number`) VALUES
-(1, '1 Brookside St', '1966-12-31', 'Test', 'F', 'TestNone', '100-222-3333'),
-(2, '2 High St', '1945-06-24', 'Test', 'M', 'TestBorderline', '200-333-4444'),
-(3, '3 Club Road', '2004-06-18', 'Test', 'M', 'TestInDanger', '300-444-5555'),
-(4, '4 Valley Dr', '2002-06-28', 'Test', 'F', 'TestEarlyOnset', '400-555-6666');
+(1, '2 Warren Street ', '1968-06-22', 'Lucas', 'M', 'Ferguson', '387-866-1399'),
+(2, '745 West Valley Farms Drive', '1952-09-27', 'Pippa', 'F', 'Rees', '628-423-0993'),
+(3, '599 East Garden Ave ', '1952-11-11', 'Edward', 'M', 'Arnold', '123-727-2779'),
+(4, '894 Hall Street', '1946-11-26', 'Anthony', 'M', 'Sharp', '451-761-8383'),
+(5, '4 Southampton Road', '1958-06-29', 'Wendy', 'F', 'Ince', '802-911-9975'),
+(6, '40 Sulphur Springs Dr', '1949-12-07', 'Tracey', 'F', 'Ross', '131-396-5049'),
+(7, '12 Cobblestone St ', '1966-12-31', 'Claire', 'F', 'Wilson', '300-452-1091'),
+(8, '193 Vale St ', '1945-06-24', 'Max', 'M', 'Buckland', '833-534-0864'),
+(9, '12 Beechwood Road', '1964-06-18', 'Natalie', 'F', 'Clark', '241-467-9197'),
+(10, '1202 Bumble Dr ', '1959-06-28', 'Piers', 'M', 'Bailey', '747-815-0557');
